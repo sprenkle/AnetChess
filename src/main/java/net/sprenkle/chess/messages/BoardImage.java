c9a7fd0322c562b4ac183aec2b07939280eb65c4
David Sprenkle <david@sprenle.net>
2017-06-11 05:58:22 +0000

    Added AnetBoardController

--- src/main/java/net/sprenkle/chess/messages/BoardImage.java
+++ src/main/java/net/sprenkle/chess/messages/BoardImage.java
@@ -6,20 +6,41 @@
 package net.sprenkle.chess.messages;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
  * @author david
  */
 public class BoardImage implements Serializable {
-    private final BufferedImage bi;
-    
-    public BoardImage(BufferedImage bi){
-        this.bi = bi;
+
+    private byte[] imageInByte;
+
+    public BoardImage(BufferedImage bi) {
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ImageIO.write(bi, "jpg", baos);
+            baos.flush();
+            imageInByte = baos.toByteArray();
+        } catch (IOException ex) {
+            Logger.getLogger(BoardImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    public BufferedImage GetBi(){
-        return this.bi;
+
+    public BufferedImage GetBi() {
+        try {
+            InputStream in = new ByteArrayInputStream(imageInByte);
+            return ImageIO.read(in);
+        } catch (IOException ex) {
+            Logger.getLogger(BoardImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
